e85f8258beff799690e457b958db2bd090d6ee8e
FilipeeX <filipeexbusinessemail@gmail.com>
2023-12-10 00:03:21 +0100

    Test animácie.

--- max7219_hodiny.ino
+++ max7219_hodiny.ino
@@ -11,13 +11,21 @@
 #define cs 5
 
 #define dataIn2 A0
-#define cs2 A2
 #define clock2 A1
+#define cs2 A2
 
 #define tlacidloZacat 10
 #define tlacidloPlus 9
 #define tlacidloMinus 8
 #define tlacidloHotovo 7
+
+Sprite startAnimacia[7] = {Sprite(8, 8, B00000000, B00000000, B00000000, B00011000, B00011000, B00000000, B00000000, B00000000), 
+                        Sprite(8, 8, B00000000, B00000000, B00011000, B00111100, B00111100, B00011000, B00000000, B00000000), 
+                        Sprite(8, 8, B00000000, B00011000, B00111100, B01111110, B01111110, B00111100, B00011000, B00000000), 
+                        Sprite(8, 8, B00011000, B00111100, B01111110, B11111111, B11111111, B01111110, B00111100, B00011000), 
+                        Sprite(8, 8, B00111100, B01111110, B11111111, B11111111, B11111111, B11111111, B01111110, B00111100), 
+                        Sprite(8, 8, B01111110, B11111111, B11111111, B11111111, B11111111, B11111111, B11111111, B01111110), 
+                        Sprite(8, 8, B11111111, B11111111, B11111111, B11111111, B11111111, B11111111, B11111111, B11111111)};
 
 
 Matrix led = Matrix(dataIn, clock, cs, pocetDisplayov);
@@ -46,12 +54,29 @@ void setup() {
 
 void loop() {
 
-  for (int i = 0; i < 4; i++) {
-    led2.write(i*8, 0, plny);
-    led.write(i*8, 0, plny);
-    delay(1000);
-    led.clear();
-    led2.clear();
-  }
+  for (int i = 0; i < 7; i++) {
+    for (int x = 0; x < 4; x++) {
+
+      int tlacidla[4] = {tlacidloZacat, tlacidloPlus, tlacidloMinus, tlacidloHotovo};
+
+      if (digitalRead(tlacidla[x]) == LOW) {
+
+        led.write(x*8, 0, startAnimacia[i]);
+        delay(1);
+        led2.write(x*8, 0, startAnimacia[i]);
+
+      } else {
+
+        led.write(x*8, 0, prazdny);
+        delay(1);
+        led2.write(x*8, 0, prazdny);
+
+      }
+
+    }
+
+    delay(10);
+
+  } 
 
 }
