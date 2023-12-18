cd68348883e34faf777929faec713961cdefaa6b
FilipeeX <filipeexbusinessemail@gmail.com>
2023-12-18 22:41:39 +0100

    Dokončenie časovo zobrazovacej funkcie.

--- max7219_hodiny.ino
+++ max7219_hodiny.ino
@@ -1,9 +1,9 @@
+#include <DS3231.h>
+#include <Wire.h>
+
 #include <Sprite.h>
 #include <binary.h>
 #include <Matrix.h>
-
-#include <stdio.h>
-#include <string.h>
 
 
 // Pocet displayov na 1 pasik (serivovo zapojeny).
@@ -22,6 +22,11 @@
 #define tlacidloMinus 8
 #define tlacidloHotovo 7
 
+
+DS3231 rtc;
+
+bool century = true;
+bool h12flag, pmFlag;
 
 Matrix led = Matrix(dataIn, clock, cs, pocetDisplayov);
 Matrix led2 = Matrix(dataIn2, clock2, cs2, pocetDisplayov);
@@ -55,24 +60,24 @@ Sprite cifra4[2] = {
   Sprite(8, 8, B00011000, B00011000, B00011000, B00011000, B00011000, B00011000, B00011000, B00111111)
 };
 Sprite cifra5[2] = {
-  Sprite(8, 8, B1),
-  Sprite(8, 8, B1)
+  Sprite(8, 8, B00111111, B00000011, B00000011, B00000011, B00000011, B00000011, B00111111, B00111111),
+  Sprite(8, 8, B00111111, B00111111, B00110000, B00110000, B00110000, B00110000, B00110000, B00111111)
 };
 Sprite cifra6[2] = {
-  Sprite(8, 8, B1),
-  Sprite(8, 8, B1)
+  Sprite(8, 8, B00111111, B00000011, B00000011, B00000011, B00110011, B00110011, B00111111, B00111111),
+  Sprite(8, 8, B00111111, B00111111, B00110011, B00110011, B00110011, B00110011, B00110011, B00111111)
 };
 Sprite cifra7[2] = {
-  Sprite(8, 8, B1),
-  Sprite(8, 8, B1)
+  Sprite(8, 8, B00111110, B00111110, B00011000, B00011000, B00011000, B00011000, B00011111, B00011111),
+  Sprite(8, 8, B00011000, B00011000, B00011000, B00011000, B00011000, B00011000, B00011000, B00011000)
 };
 Sprite cifra8[2] = {
-  Sprite(8, 8, B1),
-  Sprite(8, 8, B1)
+  Sprite(8, 8, B00111111, B00110011, B00110011, B00110011, B00110011, B00110011, B00111111, B00111111),
+  Sprite(8, 8, B00111111, B00111111, B00110011, B00110011, B00110011, B00110011, B00110011, B00111111)
 };
 Sprite cifra9[2] = {
-  Sprite(8, 8, B1),
-  Sprite(8, 8, B1)
+  Sprite(8, 8, B00111111, B00110011, B00110011, B00110011, B00110011, B00110011, B00111111, B00111111),
+  Sprite(8, 8, B00111111, B00111111, B00110000, B00110000, B00110000, B00110000, B00110000, B00111111)
 };
 
 Sprite startAnimacia[7] = {
@@ -84,7 +89,7 @@ Sprite startAnimacia[7] = {
   Sprite(8, 8, B00000000, B11111111, B11111111, B11111111, B11111111, B11111111, B11111111, B00000000),
   Sprite(8, 8, B11111111, B11111111, B11111111, B11111111, B11111111, B11111111, B11111111, B11111111),
 };
-Sprite loadingAnimacia[22] = {
+Sprite loadingAnimacia[23] = {
   Sprite(8, 8, B00000000, B00000000, B00000000, B00000000, B00000000, B00000011, B00000011, B00000000),
   Sprite(8, 8, B00000000, B00000000, B00000000, B00000000, B00000000, B00000111, B00000111, B00000000), 
   Sprite(8, 8, B00000000, B00000000, B00000000, B00000000, B00000000, B00001111, B00001111, B00000000), 
@@ -106,6 +111,7 @@ Sprite loadingAnimacia[22] = {
   Sprite(8, 8, B00000000, B00000000, B00000011, B00000011, B00000011, B00000011, B00000011, B00000000),
   Sprite(8, 8, B00000000, B00000000, B00000000, B00000011, B00000011, B00000011, B00000011, B00000000),
   Sprite(8, 8, B00000000, B00000000, B00000000, B00000000, B00000011, B00000011, B00000011, B00000000),
+  Sprite(8, 8, B00000000, B00000000, B00000000, B00000000, B00000000, B00000011, B00000011, B00000000),
   Sprite(8, 8, B00000000, B00000000, B00000000, B00000000, B00000000, B00000011, B00000011, B00000000)
 };
 
@@ -115,12 +121,14 @@ int pocet = 0;
 
 void setup() {
 
-  Serial.begin(9600);
+  // Serial.begin(9600);
 
   pinMode(tlacidloZacat, INPUT_PULLUP);
   pinMode(tlacidloPlus, INPUT_PULLUP);
   pinMode(tlacidloMinus, INPUT_PULLUP);
   pinMode(tlacidloHotovo, INPUT_PULLUP);
+
+  Wire.begin();
   
   led.clear();
   led2.clear();
@@ -138,7 +146,7 @@ void setup() {
   led.clear();
   led2.clear();
 
-  for (int i = 0; i < 22; i++) {
+  for (int i = 0; i < 23; i++) {
     for (int x = 0; x < 4; x++) {
 
       led2.write(x * 8, 0, loadingAnimacia[i]);
@@ -150,23 +158,111 @@ void setup() {
   led.clear();
   led2.clear();
 
-  led.write(0, 0, cifra4[0]);
-  led2.write(0, 0, cifra4[1]);
-
-  led.write(8, 0, cifra1[0]);
-  led2.write(8, 0, cifra1[1]);
-
-  led.write(16, 0, cifra2[0]);
-  led2.write(16, 0, cifra2[1]);
-
-  led.write(24, 0, cifra3[0]);
-  led2.write(24, 0, cifra3[1]);
-
 }
   
 
 void loop() {
 
-  //
+  int month = rtc.getMonth(century);
+  int day = rtc.getDate();
+    
+  int hours = rtc.getHour(h12flag, pmFlag);
+  int minutes = rtc.getMinute();
+
+  int seconds = rtc.getSecond();
+
+  hodiny(hours);
+  minuty(minutes);
+
+  delay(100);
+
+}
+
+
+void hodiny(int hodnota) {
+
+  if (hodnota < 10) {
+    zobraz(0, -1);
+    zobraz(1, hodnota);
+  } else {
+    zobraz(0, hodnota / 10);
+    zobraz(1, hodnota % 10);
+  }
+
+}
+
+
+void minuty(int hodnota) {
+
+  if (hodnota < 10) {
+    zobraz(2, 0);
+    zobraz(3, hodnota);
+  } else {
+    zobraz(2, hodnota / 10);
+    zobraz(3, hodnota % 10);
+  }
+
+}
+
+
+void zobraz(int segment, int cifra) {
+
+  switch (cifra) {
+
+    case 0:
+      led.write(segment * 8, 0, cifra0[0]);
+      led2.write(segment * 8, 0, cifra0[1]);
+      break;
+    
+    case 1:
+      led.write(segment * 8, 0, cifra1[0]);
+      led2.write(segment * 8, 0, cifra1[1]);
+      break;
+    
+    case 2:
+      led.write(segment * 8, 0, cifra2[0]);
+      led2.write(segment * 8, 0, cifra2[1]);
+      break;
+    
+    case 3:
+      led.write(segment * 8, 0, cifra3[0]);
+      led2.write(segment * 8, 0, cifra3[1]);
+      break;
+    
+    case 4:
+      led.write(segment * 8, 0, cifra4[0]);
+      led2.write(segment * 8, 0, cifra4[1]);
+      break;
+    
+    case 5:
+      led.write(segment * 8, 0, cifra5[0]);
+      led2.write(segment * 8, 0, cifra5[1]);
+      break;
+    
+    case 6:
+      led.write(segment * 8, 0, cifra6[0]);
+      led2.write(segment * 8, 0, cifra6[1]);
+      break;
+    
+    case 7:
+      led.write(segment * 8, 0, cifra7[0]);
+      led2.write(segment * 8, 0, cifra7[1]);
+      break;
+
+    case 8:
+      led.write(segment * 8, 0, cifra8[0]);
+      led2.write(segment * 8, 0, cifra8[1]);
+      break;
+    
+    case 9:
+      led.write(segment * 8, 0, cifra9[0]);
+      led2.write(segment * 8, 0, cifra9[1]);
+      break;
+    
+    default:
+      led.write(segment * 8, 0, prazdny);
+      led2.write(segment * 8, 0, prazdny);
+    
+  }
 
 }
